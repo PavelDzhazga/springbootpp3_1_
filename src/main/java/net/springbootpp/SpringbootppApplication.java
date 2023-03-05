51166a4fc5167a253ceb426a30a476203672c5ff
Pavel Dzhazga <dzhazga2002@mail.ru>
2023-03-05 14:22:26 +0300

    ава

---
+++ src/main/java/net/springbootpp/SpringbootppApplication.java
@@ -0,0 +1,13 @@
+package net.springbootpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootpp/SpringbootppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
